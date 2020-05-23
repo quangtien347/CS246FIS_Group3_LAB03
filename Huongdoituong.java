72268c97107a882bca4d5d77a12f127f87a74c64
quangtien347 <65383812+quangtien347@users.noreply.github.com>
2020-05-23 13:49:16 +0700

    Add files via upload

---
+++ Huongdoituong.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huongdoituong;
+import huongdoituong.Person;
+/**
+ *
+ * @author ADMIN
+ */
+public class Huongdoituong extends Person{
+    protected Huongdoituong(String name, int age , float height){
+        super (name, age , height);
+    }
+    public static void main(String[] args) {
+        Huongdoituong a = new Huongdoituong("tien",19,1.7f);
+        a.eat(" Rice");
+        int age =a.getAge();
+        System.out.println("His age:"+age);
+    }
+    
+}
+
