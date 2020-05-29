bbe460fe4955c1c8349a8f45e7d747cd135d0415
nhatduy51 <60657954+nhatduy51@users.noreply.github.com>
2020-05-29 16:25:32 +0700

    Đã xong class CalculatorHelp

--- CalculatorHelp.java
+++ CalculatorHelp.java
@@ -11,5 +11,17 @@ import static javafx.scene.input.KeyCode.R;
 
 public class CalculatorHelp extends Activity {
 
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.calculator_help);
+		Button btBack = (Button) findViewById(R.id.back);
+		btBack.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),Calculator.class);
+				startActivity(intent);
+			}
+		});
+	}
 }
